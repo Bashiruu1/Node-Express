15d510f7da793e236dc351fc77dc02abaec48a67
Bashiruu1 <silvergold21@gmail.com>
2022-02-17 17:56:01 -0500

    Added bodyParser module to enable reading body Also added app.post to filter post requests, similiarly app.put, app.delete app.patch can be used for other HTTP Verbs

--- Express-Work/server.js
+++ Express-Work/server.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('I am in first middleware')
-  res.send('<h1>Hello from express</h1>')
-  next()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/add-product', (req, res, next) => {
+  console.log('I am add-product middleware')
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title">
+      <button type="submit">Add Product</button>
+    </form>
+  `)
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+  console.log('In Home Directory')
+  res.end()
 })
 
 app.listen(3000)
