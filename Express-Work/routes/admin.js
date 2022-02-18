84b532d3c2f6924f74c983b2b490916057a3d82a
Bashiruu1 <silvergold21@gmail.com>
2022-02-17 23:46:27 -0500

    Filtering admin path

--- Express-Work/routes/admin.js
+++ Express-Work/routes/admin.js
@@ -4,14 +4,14 @@ const router = express.Router()
 router.get('/add-product', (req, res, next) => {
   console.log('I am add-product middleware')
   res.send(`
-    <form action="/product" method="POST">
+    <form action="/admin/product" method="POST">
       <input type="text" name="title">
       <button type="submit">Add Product</button>
     </form>
   `)
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
   res.redirect('/')
 })
